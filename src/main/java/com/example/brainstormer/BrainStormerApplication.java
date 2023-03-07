6576db9af7ae29988424736dd29813fcd3333f7a
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-03-07 10:32:43 +0100

    initial commit

---
+++ src/main/java/com/example/brainstormer/BrainStormerApplication.java
@@ -0,0 +1,13 @@
+package com.example.brainstormer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainStormerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainStormerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brainstormer/BrainStormerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brainstormer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainStormerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
