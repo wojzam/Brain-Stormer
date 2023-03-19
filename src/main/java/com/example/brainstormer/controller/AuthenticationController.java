bb0681916835c6feccbb7e143a64dc7921ed9565
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-03-19 11:19:24 +0100

    change http code at /register

--- src/main/java/com/example/brainstormer/controller/AuthenticationController.java
+++ src/main/java/com/example/brainstormer/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.example.brainstormer.dto.RegisterRequest;
 import com.example.brainstormer.service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,7 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(authenticationService.register(request));
     }
 
     @PostMapping("/authenticate")
