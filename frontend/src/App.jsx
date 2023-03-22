558ab9059b58da460071c2a55ba1a6149f602e61
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-03-22 13:29:58 +0100

    add Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Create from "./pages/Create";
 import Topic from "./pages/Topic";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/topic" element={<Topic />} />
-      </Routes>
+      <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/topic" element={<Topic />} />
+        </Routes>
+      </main>
     </>
   );
 }
