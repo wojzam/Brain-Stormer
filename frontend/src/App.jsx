8980753311ef78d4c6e3367d399af1f0cbef4ad5
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-03-22 13:32:27 +0100

    add basic login and sign up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 import Explore from "./pages/Explore";
 import Create from "./pages/Create";
 import Topic from "./pages/Topic";
@@ -12,6 +14,8 @@ export default function App() {
       <main className="content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/create" element={<Create />} />
           <Route path="/topic" element={<Topic />} />
