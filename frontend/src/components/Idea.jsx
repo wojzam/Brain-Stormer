156362f07853170e20f1ab2ad1f3466a15b6f420
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-04-03 15:52:17 +0200

    add basic voting functionality

--- frontend/src/components/Idea.jsx
+++ frontend/src/components/Idea.jsx
@@ -4,18 +4,64 @@ import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
 import EditIcon from "@mui/icons-material/Edit";
 
-export default function Idea({ title, description }) {
+export default function Idea({ id, title, description, votes }) {
   const [likeClicked, setLikeClicked] = useState(false);
   const [dislikeClicked, setDislikeClicked] = useState(false);
+  const [localVotes, setLocalVotes] = useState(0);
+
+  const handleVote = (voteValue) => {
+    const token = localStorage.getItem("token");
+    fetch(`/api/idea/${id}/vote?value=${voteValue}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(() => setLocalVotes(voteValue));
+  };
+
+  const deleteVote = () => {
+    const token = localStorage.getItem("token");
+    fetch(`/api/idea/${id}/vote`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(() => setLocalVotes(0));
+  };
 
   const handleLikeClick = () => {
-    setLikeClicked(true);
-    setDislikeClicked(false);
+    if (likeClicked) {
+      setLikeClicked(false);
+      deleteVote();
+    } else {
+      setLikeClicked(true);
+      setDislikeClicked(false);
+      handleVote(1);
+    }
   };
 
   const handleDislikeClick = () => {
-    setDislikeClicked(true);
-    setLikeClicked(false);
+    if (dislikeClicked) {
+      setDislikeClicked(false);
+      deleteVote();
+    } else {
+      setDislikeClicked(true);
+      setLikeClicked(false);
+      handleVote(-1);
+    }
+  };
+
+  const getVotesColor = () => {
+    const sum = votes + localVotes;
+    if (sum > 0) {
+      return "green";
+    } else if (sum < 0) {
+      return "red";
+    } else {
+      return "gray";
+    }
   };
 
   return (
@@ -38,15 +84,18 @@ export default function Idea({ title, description }) {
           {title}
         </Typography>
         <Typography variant="h6">{description}</Typography>
-        <Box>
+        <Box display="flex" flexDirection="row" mt="2em">
           <IconButton
-            color={dislikeClicked ? "primary" : "default"}
+            color={dislikeClicked ? "secondary" : "default"}
             onClick={handleDislikeClick}
           >
             <ThumbDownOutlinedIcon />
           </IconButton>
+          <Typography variant="h6" color={getVotesColor}>
+            {votes + localVotes}
+          </Typography>
           <IconButton
-            color={likeClicked ? "primary" : "default"}
+            color={likeClicked ? "secondary" : "default"}
             onClick={handleLikeClick}
           >
             <ThumbUpOutlinedIcon />
