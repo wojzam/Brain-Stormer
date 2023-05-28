dff56cec4e9e2c81a00bee7d6936b346d958777c
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-05-28 15:36:00 +0200

    add asynchronous send methods for Kafka messages

--- src/main/java/com/example/brainstormer/service/IdeaService.java
+++ src/main/java/com/example/brainstormer/service/IdeaService.java
@@ -12,18 +12,23 @@ import com.example.brainstormer.repository.TopicRepository;
 import com.example.brainstormer.repository.VoteRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class IdeaService {
 
+    private final Logger logger = LoggerFactory.getLogger(IdeaService.class);
     private final IdeaRepository ideaRepository;
     private final TopicRepository topicRepository;
     private final VoteRepository voteRepository;
@@ -46,7 +51,7 @@ public class IdeaService {
                 .build();
 
         ideaRepository.save(idea);
-        kafkaTemplate.send("ideaUpdate", IdeaUpdateMessage.getCreateOf(idea, loggedInUser.getId()));
+        sendMessage(IdeaUpdateMessage.getCreateOf(idea, loggedInUser.getId()));
         IdeaDto ideaDto = new IdeaDto(idea);
         ideaDto.setCanEdit(true);
         return ResponseEntity.status(HttpStatus.CREATED).body(ideaDto);
@@ -64,14 +69,14 @@ public class IdeaService {
             idea.setDescription(description);
         }
 
-        kafkaTemplate.send("ideaUpdate", IdeaUpdateMessage.getUpdateOf(idea, loggedInUser.getId()));
+        sendMessage(IdeaUpdateMessage.getUpdateOf(idea, loggedInUser.getId()));
     }
 
     public void deleteIdea(UUID id) {
         User loggedInUser = authenticationService.getLoggedInUser();
         Idea idea = getUserIdea(loggedInUser, id);
         ideaRepository.deleteById(idea.getId());
-        kafkaTemplate.send("ideaUpdate", IdeaUpdateMessage.getDeleteOf(idea, loggedInUser.getId()));
+        sendMessage(IdeaUpdateMessage.getDeleteOf(idea, loggedInUser.getId()));
     }
 
     @Transactional
@@ -94,7 +99,7 @@ public class IdeaService {
 
         idea.getVotes().add(vote);
         voteRepository.save(vote);
-        kafkaTemplate.send("ideaUpdate", IdeaUpdateMessage.getVoteOf(idea, loggedInUser.getId(), value - previousVoteValue));
+        sendMessage(IdeaUpdateMessage.getVoteOf(idea, loggedInUser.getId(), value - previousVoteValue));
     }
 
     @Transactional
@@ -109,7 +114,7 @@ public class IdeaService {
         int previousVoteValue = idea.getUserVote(loggedInUser);
 
         voteRepository.deleteAllByUserAndIdea(loggedInUser, idea);
-        kafkaTemplate.send("ideaUpdate", IdeaUpdateMessage.getVoteOf(idea, loggedInUser.getId(), -previousVoteValue));
+        sendMessage(IdeaUpdateMessage.getVoteOf(idea, loggedInUser.getId(), -previousVoteValue));
     }
 
     private Idea getUserIdea(User user, UUID ideaId) {
@@ -120,5 +125,14 @@ public class IdeaService {
         }
 
         return idea;
+    }
+
+    private void sendMessage(IdeaUpdateMessage ideaUpdateMessage) {
+        CompletableFuture<SendResult<String, IdeaUpdateMessage>> future = kafkaTemplate.send("ideaUpdate", ideaUpdateMessage);
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                logger.warn("Unable to send message due to : " + ex.getMessage());
+            }
+        });
     }
 }
